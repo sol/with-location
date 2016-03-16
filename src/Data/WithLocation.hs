b48c67f6ce9c88e557823581a5139a46e363a118
Simon Hengel <sol@typeful.net>
2016-03-16 13:35:26 +0800

    Don't require users to use CPP

--- src/Data/WithLocation.hs
+++ src/Data/WithLocation.hs
@@ -1,5 +1,11 @@
 {-# LANGUAGE CPP #-}
-#include "with-location.h"
+{-# LANGUAGE Rank2Types #-}
+
+#if MIN_VERSION_base(4,8,1)
+#define HAS_SOURCE_LOCATIONS
+{-# LANGUAGE ImplicitParams #-}
+#endif
+
 module Data.WithLocation where
 
 #ifdef HAS_SOURCE_LOCATIONS
@@ -7,6 +13,12 @@ module Data.WithLocation where
 import           GHC.SrcLoc
 #endif
 import           GHC.Stack
+#endif
+
+#ifdef HAS_SOURCE_LOCATIONS
+type WithLocation a = (?loc :: CallStack) => a
+#else
+type WithLocation a = a
 #endif
 
 data Location = Location {
@@ -17,7 +29,7 @@ data Location = Location {
 , locationColumn :: Int
 } deriving (Eq, Ord, Show)
 
-location :: with_location Maybe Location
+location :: WithLocation (Maybe Location)
 #ifdef HAS_SOURCE_LOCATIONS
 location = case reverse (getCallStack ?loc) of
   (_, loc) : _ -> Just $ Location (srcLocPackage loc) (srcLocModule loc) (srcLocFile loc) (srcLocStartLine loc) (srcLocStartCol loc)
